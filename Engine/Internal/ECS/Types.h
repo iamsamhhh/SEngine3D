cc3b1918ce3e16f5ccd98c243e8b2b33fd6f4f5f
iamsamhhh <sc_samchen@icloud.com>
2022-04-03 20:29:40 +0800

    Update Types.h

--- Engine/Internal/ECS/Types.h
+++ Engine/Internal/ECS/Types.h
@@ -9,13 +9,13 @@
 using Entity = std::uint32_t;
 
 // Used to define the size of arrays later on
-const Entity MAX_ENTITIES = 5000;
+#define MAX_ENTITIES = 5000;
 
 // A simple type alias
 using ComponentType = std::uint8_t;
 
 // Used to define the size of arrays later on
-const ComponentType MAX_COMPONENTS = 32;
+#define MAX_COMPONENTS = 32;
 
 // A simple type alias
 using Signature = std::bitset<MAX_COMPONENTS>;
