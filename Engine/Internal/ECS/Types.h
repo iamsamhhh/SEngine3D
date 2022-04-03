4add9e17e0df81243cc3e974298122cc057be652
iamsamhhh <sc_samchen@icloud.com>
2022-04-03 20:27:47 +0800

    Init commit for ECS

---
+++ Engine/Internal/ECS/Types.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <queue>
+#include <array>
+#include <bitset>
+#include <cstdint>
+
+// A simple type alias
+using Entity = std::uint32_t;
+
+// Used to define the size of arrays later on
+const Entity MAX_ENTITIES = 5000;
+
+// A simple type alias
+using ComponentType = std::uint8_t;
+
+// Used to define the size of arrays later on
+const ComponentType MAX_COMPONENTS = 32;
+
+// A simple type alias
+using Signature = std::bitset<MAX_COMPONENTS>;
